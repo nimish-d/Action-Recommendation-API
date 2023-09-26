b94e9bab396bcc59865b11914f2ecf37300b1bb8
nimish-d <nimish.d@ihx.in>
2023-09-26 09:37:54 +0000

    predefined return datatype form `get_filtered_record` method

--- app/controllers/fetch_records.py
+++ app/controllers/fetch_records.py
@@ -1,11 +1,13 @@
+from app.core import logger
 from app.core import cfg
+from app.schema.Action_Recommendation_API import ActionRecommendation
 
-def get_filtered_record(db_connector, tablename, filters):
+def get_filtered_record(db_connector, tablename, filters) -> ActionRecommendation:
         ICD = filters['ICD']
         PayerId = filters['PayerId']
         query = f"""SELECT * FROM {tablename} WHERE ("ICD"='{ICD}' AND "PayerId"='{PayerId}')"""
         record = db_connector.execute_query(query)[0]
         result = {}
         result['TopActionList'] = [int(i) for i in list(record['Actions'])][0:cfg['Number_of_Actions_Recommended']]
-        result['Text'] = 'Text'
-        return result
+        result['Text'] = "Text"
+        return ActionRecommendation(**result)
